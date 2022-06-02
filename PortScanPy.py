a6e0a854d7f48665afb4e3b688e5c948506d8821
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-02 00:37:40 -0300

    Update PortScanPy.py

--- PortScanPy.py
+++ PortScanPy.py
@@ -1,5 +1,5 @@
-import socket
-import sys
+import socket      #PortScan que scaneia as portas de acordo com uma lista usando um For loop, retorna se a porta está aberta e se tiver, o banner
+import sys         #serviço
 
 
 portas_lis = [20, 21, 22, 23, 25, 80, 443, 110, 3389]  #Lista para as portas
