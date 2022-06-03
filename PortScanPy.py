295e643203232c709ae45dd1b22c05e1bf56bfce
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-03 17:44:30 -0300

    Update PortScanPy.py

--- PortScanPy.py
+++ PortScanPy.py
@@ -1,18 +1,38 @@
-import socket      #PortScan que scaneia as portas de acordo com uma lista usando um For loop, retorna se a porta está aberta e se tiver, o banner
-import sys         #serviço
+#Meu primeiro programa em Python, um portscan básico que é baseado numa lista de portas TCP populares
+#Comecei dia 26 e enrrolei, dia 03 de Junho dei uma pesquisado num problema e consegui arrumar o timeout etc
+#Apenas diz se a porta está aberta e se tiver, passa um banner do serviço rodando na porta
+#GitHub: github.com/paixaoalmeida
+#Todo o código está comentado pois preciso lembrar das coisas mais básicas!
+#Ultima alteração dia 03 de Junho ás 17:02 #No momento apenas dá a porta aberta e serviços rodando, mas de maneira limpa
 
 
-portas_lis = [20, 21, 22, 23, 25, 80, 443, 110, 3389]  #Lista para as portas
+import socket
+import sys
+import time
 
-for portas in portas_lis: #For loop para ler a lista e fazer as conexões
-    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #biblioteca socket
-    res = meusocket.connect_ex((sys.argv[1],portas))
-    
-    if (res == 0):  #Se for 0 então a porta está ativa
+ip = sys.argv[1]
+
+listaportas = [20, 21, 22, 23, 25, 80, 110, 139, 443, 445]  #Lista para as portas
+
+print("O script tem um intervalo de 10 segundos em cada porta! \n")
+for portas in listaportas:  #For loop para ler a lista e fazer as conexões
+    meusocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #biblioteca socket
+    meusocket.settimeout(10)  #Timeout para conectar nas portas (p nao ficar eternamente tentando conectar)
+    res = meusocket.connect_ex((ip, portas))  #Conetar o socket no host desejado, modo TCP, endereço e porta
+    time.sleep(10)    #No For loop vai dar um sleep de 10 sec, para tentar evitar ser bloqueado
+
+    if (res == 0):   #Se for 0 então a porta está ativa
         print("Porta" ,portas, "Aberta \n")
-        servico = meusocket.recv(1024) #Receber a resposta (no caso o banner da porta)
-        print(f"Serviço rodando na porta -----> {portas} ------> {servico} \n")
-        meusocket.close() #Fechar a conexão
+
+        try:
+            servico = meusocket.recv(1024)   #Receber a resposta (no caso o banner da porta)
+            encodado = servico.decode("utf-8")  #Decodando a variavel de bytes para string, saida sem sujeira!
+            print(f"Serviço rodando na porta {portas} ------> {encodado} \n")
+        except:
+            print(f"Sem banner capturado na porta {portas} \n")
+meusocket.close()
+print("Sem mais portas abertas encontradas!")
+
 
 
 
